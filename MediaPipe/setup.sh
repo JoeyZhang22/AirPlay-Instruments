e8f8d7fa8510f444462089e2a001e70c8176bbb5
nickoeco <40157334+nickoeco@users.noreply.github.com>
2024-09-08 03:18:58 -0400

    code stole from MediaPipe website

---
+++ MediaPipe/setup.sh
@@ -0,0 +1,16 @@
+# Install Python dependencies.
+python3 -m pip install pip --upgrade
+python3 -m pip install -r requirements.txt
+
+wget -O gesture_recognizer.task -q https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task
+
+# Install OpenCV libs
+apt-get install -y \
+    libopencv-core-dev \
+    libopencv-highgui-dev \
+    libopencv-calib3d-dev \
+    libopencv-features2d-dev \
+    libopencv-imgproc-dev \
+    libopencv-video-dev
+
+apt-get install -y libopencv-contrib-dev
