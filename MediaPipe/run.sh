b46155134f9de9284c4d190038e4ce7654ecbe7d
Nick Lin <40157334+nickoeco@users.noreply.github.com>
2024-09-10 20:03:04 -0400

    Start Saperate OpenCV and MediaPipe

--- MediaPipe/run.sh
+++ MediaPipe/run.sh
@@ -1,7 +1,7 @@
 NUM_HANDS=2
 MIN_HAND_DETECT_CONFIDENCE=0.5
-WIDTH=1280
-HEIGHT=720
+WIDTH=1920
+HEIGHT=1080
 
-python3 recognize.py    --model gesture_recognizer.task   --numHands $NUM_HANDS   --minHandDetectionConfidence $MIN_HAND_DETECT_CONFIDENCE\
+python3 main.py    --model gesture_recognizer.task   --numHands $NUM_HANDS   --minHandDetectionConfidence $MIN_HAND_DETECT_CONFIDENCE\
                         --frameWidth $WIDTH --frameHeight $HEIGHT
