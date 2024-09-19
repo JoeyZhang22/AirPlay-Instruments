0c8071ec9ceede59e58e3426421852971d8e34f5
Nick Lin <40157334+nickoeco@users.noreply.github.com>
2024-09-19 02:18:39 -0400

    Rebased and reorder the drawing processes to improve FPS

--- MediaPipe/run.sh
+++ MediaPipe/run.sh
@@ -1,6 +1,6 @@
 NUM_HANDS=2
 MIN_HAND_DETECT_CONFIDENCE=0.5
-WIDTH=760
+WIDTH=640
 HEIGHT=480
 
 python3 main.py    --model gesture_recognizer.task   --numHands $NUM_HANDS   --minHandDetectionConfidence $MIN_HAND_DETECT_CONFIDENCE\
