ecad177dd57d763a20901ae00d30196a10c88471
Tangia Zhou <85449931+tangiazhou@users.noreply.github.com>
2024-09-22 18:09:48 -0400

    Add Gesture class definition

--- DataProcessing/main.py
+++ DataProcessing/main.py
@@ -2,14 +2,13 @@
 
 # What is being mapped from what Nick gives us
 class Gesture:
-    __init()
-    location
-    gesture_name
-    start_time
-    end_time
-    duration
-    confidence
-    gesture_id
+    def __init__(self, location, gesture_name, start_time, end_time, confidence, gesture_id):
+        self.location = location  
+        self.gesture_name = gesture_name  # Name of the gesture
+        self.start_time = start_time  # Start time of the gesture
+        self.end_time = end_time  # End time of the gesture
+        self.confidence = confidence  # Confidence level in gesture recognition
+        self.gesture_id = gesture_id  # Unique identifier for the gesture
 
 # Object to give to Joey 
 class Chord:
