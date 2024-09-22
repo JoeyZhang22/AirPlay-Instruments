f27b39eb220be68c31eefc6f2ff7d88c3e70a692
loganclarke72 <74108316+loganclarke72@users.noreply.github.com>
2024-09-22 19:59:02 -0400

    Update main.py

--- DataProcessing/main.py
+++ DataProcessing/main.py
@@ -11,14 +11,47 @@ class Gesture:
         self.gesture_id = gesture_id  # Unique identifier for the gesture
 
 # Object to give to Joey 
-class Chord:
-    Key # C, D, E, F, ... etc.
-    chord_type # major, minor, diminished 
-    velocity 
-    duration
+chord_type_list = ["major", "minor", "dim7", "dom7"]
+notes  = ["a", "b", "c", "d", "e", "f", "g"]
+#start with natrual chords to start
+#map each defined gesture to a note
+def create_gesture_dict(gesture_list, notes_list): 
+    #for each note assign a gesture. given gesture can retrieve note
+    gesture_dict = {gesture_list[i]: notes[i] for i in range(len(notes))}
+    return gesture_dict
+#start with major, minor, dim and dom 7ths
+#match each zone to a chord type
+def create_zone_map(chord_type_list):
+    zone_map = []
+    for chord_type in chord_type_list:
+        zone_map.append(chord_type) 
+    return zone_map 
+
+####now given a gesture and given a zone can get chord note and type
+#
+class chord:
+    def __init__(self, gesture, gesture_dict, velocity, duration,  zone, zone_map):
+        self.key = gesture_dict[gesture]
+        self.chord_type = zone_map[zone]
+        self.velocity = velocity
+        self.duration = duration
+
+
+gestures = ["thumbs_up", "thumbs_down", "peace", "love", "spock", "guh", "meep"]
+
+zone_map = create_zone_map(chord_type_list)
+gesture_dict = create_gesture_dict(gestures, notes)
+
+my_first_chord = chord("thumbs_up", gesture_dict, 17, 3, 2, zone_map)
+
+print("the zone map")
+print(zone_map)
+print("the gesture dict")
+print(gesture_dict)
+print("the chord propertires:")
+print(vars(my_first_chord))
+
+
 
 # To use the difference in location to calculate the velocity of the strum
 def get_velocity(location) -> float:
-
-# Map the zone of the frame and the gesture to the key, chord type, and strum
-def gesture_map() -> None:
