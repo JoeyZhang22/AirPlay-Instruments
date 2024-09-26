bee81dd06745b37e14c5a14f1f17fb7a5598c986
Tangia <tangia.zhou@gmail.com>
2024-09-26 11:56:50 -0400

    Cleaning up DataProcessing Branch and adding a README

---
+++ DataProcessing/README.md
@@ -0,0 +1,3 @@
+Data Processing
+
+This section focuses on translating the movements detected by the camera into chords and their durations
--- DataProcessing/main.py
+++ DataProcessing/main.py
@@ -6,26 +6,39 @@ from ../MIDI/usage/midi_mingus.py import *
 class Gesture:
     def __init__(self, location, gesture_name, start_time, end_time, confidence, gesture_id):
         self.location = location  
+        self.start_time = start_time # Start time of strum
+        self.end_time = end_time # End time of strum
         self.gesture_name = gesture_name  # Name of the gesture
-        self.start_time = start_time  # Start time of the gesture
-        self.end_time = end_time  # End time of the gesture
         self.confidence = confidence  # Confidence level in gesture recognition
         self.gesture_id = gesture_id  # Unique identifier for the gesture
 
-# Object to give to Joey 
 chord_type_list = ["major", "minor", "dim7", "dom7"]
-notes  = ["a", "b", "c", "d", "e", "f", "g"]
+chord_natural  = ["C", "D", "E", "F", "G", "A", "B"]
+chord_sharp = ["C#", "D#", "F#", "G#", "A#"]
+chord_flat = ["Db", "Eb", "Gb", "Ab", "Bb"]
 
 #to play chords using Joey's MIDI stuff
 chord_list = ["Cmin7", "F7", "Gmaj", "Cmaj"]
 
-#start with natrual chords to start
+# Process the gesture sent by Nick
+# Input: Nick's Output dictionary containing Handedness, Area, Gesture Type, Score (confidence)
+def process_gestures(Output: input): 
+    action = []
+
+    # Map Nick's output object to Gesture object
+    action_temp = Gesture(
+        location = input.gesture_landmarks,
+        gesture_name = input.gesture_type,
+        confidence = input.score
+    )
+    # The rest of this function should store the actions and determine when a strum is made
+
 #map each defined gesture to a note
-def create_gesture_dict(gesture_list, notes_list): 
+def create_gesture_dict(gesture_list, chord_list): 
     #for each note assign a gesture. given gesture can retrieve note
-    gesture_dict = {gesture_list[i]: notes[i] for i in range(len(notes))}
+    gesture_dict = {gesture_list[i]: chord[i] for i in range(len(chord))}
     return gesture_dict
-#start with major, minor, dim and dom 7ths
+
 #match each zone to a chord type
 def create_zone_map(chord_type_list):
     zone_map = []
@@ -33,20 +46,17 @@ def create_zone_map(chord_type_list):
         zone_map.append(chord_type) 
     return zone_map 
 
-####now given a gesture and given a zone can get chord note and type
-#
+#now given a gesture and given a zone can get chord note and type
 class chord:
     def __init__(self, gesture, gesture_dict, velocity, duration,  zone, zone_map):
         self.key = gesture_dict[gesture]
         self.chord_type = zone_map[zone]
         self.velocity = velocity
-        self.duration = duration
-
 
 gestures = ["thumbs_up", "thumbs_down", "peace", "love", "spock", "guh", "meep"]
 
 zone_map = create_zone_map(chord_type_list)
-gesture_dict = create_gesture_dict(gestures, notes)
+gesture_dict = create_gesture_dict(gestures, chord)
 
 my_first_chord = chord("thumbs_up", gesture_dict, 17, 3, 2, zone_map)
 
@@ -58,7 +68,8 @@ print("the chord propertires:")
 print(vars(my_first_chord))
 
 # To use the difference in location to calculate the velocity of the strum
-def get_velocity(location, start_time, end_time):
+# Get the object where the strum is at either a maxima and minima n a certain divison of the screen
+def get_velocity(location):
     velocities = []
     for i in range(1, len(location)):
         # Extracting x and y coordinates for the wrist landmark (index 0)
