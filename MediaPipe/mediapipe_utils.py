8859acd8e29788a5b8f3c66a05b4d5ec0a08a848
Nick Lin <40157334+nickoeco@users.noreply.github.com>
2024-10-15 03:23:50 -0400

    Adding timestamp for each recognition output, and now each ouput is for each  frame instead of each hand

--- MediaPipe/mediapipe_utils.py
+++ MediaPipe/mediapipe_utils.py
@@ -1,5 +1,6 @@
 # Scripts to run MediaPipe for gesture recognition.
 import copy
+import time
 
 import mediapipe as mp
 
@@ -30,6 +31,7 @@ class Recognizer:
             timestamp_ms: int,
         ):
             self.recognition_result_list.append(result)
+            self.timestamp_second = time.time()
 
         # Initialize the gesture recognizer model
         self.recognition_result_list = []
@@ -44,6 +46,7 @@ class Recognizer:
             result_callback=save_result,
         )
         self.recognizer = vision.GestureRecognizer.create_from_options(self.options)
+        self.timestamp_second = time.time()
 
     # To be implemented...
     def run_sync(self, rgb_image):
@@ -56,18 +59,59 @@ class Recognizer:
         self.recognizer.recognize_async(mp_image, time_ms)
 
     # Recognized result getter
-    def get_recognized_output(self):
-        transformed_output = self.transform_recognized_output()
+    def get_recognized_output(self, areas):
+        # If there is no new recognition result return immediately
+        if not self.recognition_result_list:
+            return []
+        
+        transformed_output = self.transform_recognized_output(areas)
         self.recognition_result_list.clear()
         return transformed_output
 
     # This function transform the output from the MP recognizer to the input for Decision Block
-    def transform_recognized_output(
-        self,
-    ):
-        # transform_output = dict()
-        transform_output = copy.deepcopy(self.recognition_result_list)
-        return transform_output
+    def transform_recognized_output(self, areas):
+        """
+        Output for decision block format:
+        Output = {
+        "Handedness" :
+        "Area" :
+        "Gesture_Type" :
+        "Gesture_Landmarks" :
+        "Score" :
+        "Time" :
+        }
+        """
+
+        recognition_results = copy.deepcopy(self.recognition_result_list)
+    
+        transformed_results = []
+        for recognition_result in recognition_results:
+            transformed_outputs = []
+            # Transform the results for decision box
+            for hand_index, hand_landmarks in enumerate(recognition_result.hand_landmarks):
+                transformed_output = dict()
+
+                transformed_output["Handedness"] = recognition_result.handedness[hand_index][0].category_name
+                transformed_output["Gesture_Type"] = recognition_result.gestures[hand_index][0].category_name
+                transformed_output["Gesture_Landmarks"] = hand_landmarks
+                transformed_output["Score"] = round(
+                    recognition_result.gestures[hand_index][0].score, 2
+                )
+                transformed_output["Time"] = self.timestamp_second
+
+                transformed_output["Area"] = "None"
+
+                # determine which area this head appears at
+                for area in areas:
+                    if area.is_within(hand_landmarks):
+                        transformed_output["Area"] = area.name
+                        break
+
+                transformed_outputs.append(transformed_output)
+            
+            transformed_results.append(transformed_outputs)
+
+        return transformed_results
 
     # Close recognizer
     def close(self):
@@ -94,45 +138,6 @@ def draw_landmarks(image, hand_landmarks):
     )
 
 
-# This function transform the output from the MP recognizer to the input for Decision Block
-def transform_recognition_output(recognition_result, areas):
-    """
-    Output for decision block format:
-    Output = {
-      "Handedness" :
-      "Area" :
-      "Gesture_Type" :
-      "Gesture_Landmarks" :
-      "Score" :
-    }
-    """
-    transformed_outputs = []
-
-    # Transform the results for decision box
-    for hand_index, hand_landmarks in enumerate(recognition_result.hand_landmarks):
-        transformed_output = dict()
-
-        transformed_output["Handedness"] = recognition_result.handedness[hand_index][
-            0
-        ].category_name
-        transformed_output["Gesture_Type"] = recognition_result.gestures[hand_index][
-            0
-        ].category_name
-        transformed_output["Gesture_Landmarks"] = hand_landmarks
-        transformed_output["Score"] = round(
-            recognition_result.gestures[hand_index][0].score, 2
-        )
-
-        transformed_output["Area"] = "None"
-        # determine which area this head appears at
-        for area in areas:
-            if area.is_within(hand_landmarks):
-                transformed_output["Area"] = area.name
-                break
-
-        transformed_outputs.append(transformed_output)
-
-    return transformed_outputs
 
 
 # If more than this within_percentage of the hand appeared in the area, then return true for Area.is_within()
