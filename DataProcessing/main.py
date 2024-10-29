9a488fb79011c7467e461fdbd7d03f115bd4739f
Tangia Zhou <85449931+tangiazhou@users.noreply.github.com>
2024-10-28 23:04:33 -0400

    Update main.py with gesture processing function

--- DataProcessing/main.py
+++ DataProcessing/main.py
@@ -12,26 +12,51 @@ class Gesture:
         self.confidence = confidence  # Confidence level in gesture recognition
         self.gesture_id = gesture_id  # Unique identifier for the gesture
 
-chord_type_list = ["major", "minor", "dim7", "dom7"]
-chord_natural  = ["C", "D", "E", "F", "G", "A", "B"]
-chord_sharp = ["C#", "D#", "F#", "G#", "A#"]
-chord_flat = ["Db", "Eb", "Gb", "Ab", "Bb"]
+# Object to be sent to Joey
+class Chord:
+    def __init__(self, gesture_dict, gesture, chord_type, duration):
+        self.key = gesture_dict[gesture]
+        self.chord_type = chord_type
+        self.duration = duration
 
-#to play chords using Joey's MIDI stuff
-chord_list = ["Cmin7", "F7", "Gmaj", "Cmaj"]
+gesture_dict = {
+    "open_hand": 'C',
+    "fist": 'D',          
+    "pointing": 'E',      
+    "peace_sign": 'F',    
+    "thumbs_up": 'G',     
+    "thumbs_down": 'A',  
+    "rock_on": 'B',       
+    "ok_sign": 'C'        
+}
+
+test_list = []
 
 # Process the gesture sent by Nick
-# Input: Nick's Output dictionary containing Handedness, Area, Gesture Type, Score (confidence)
-def process_gestures(Output: input): 
-    action = []
+# Input: Nick's output dictionary containing Handedness, Area, Gesture Type, Score
+# Returns: Chord object containing Key, Chord Type (Major, Minor, Special), Duration
+def process_gestures(Gesture: Gesture): 
+    prev_gesture = []
 
-    # Map Nick's output object to Gesture object
-    action_temp = Gesture(
-        location = input.gesture_landmarks,
-        gesture_name = input.gesture_type,
-        confidence = input.score
-    )
-    # The rest of this function should store the actions and determine when a strum is made
+    if Gesture.Handedness is "Right" and Gesture.confidence > 70:
+        gesture = Gesture.gesture_type
+        Chord.key = gesture_dict.get(gesture)
+        start_time = Gesture.Time
+
+        if Gesture.location is "Major":
+            Chord.chord_type = "major"
+        elif Gesture.location is "Minor":
+            Chord.chord_type = "minor"
+        else:
+            Chord.chord_type = "dom7"
+
+        # If it is a different gesture or in a different part of the screen, take the current time and calculate the duration of the gesture
+        if (gesture != prev_gesture.gesture and prev_gesture != None) or (gesture.location != prev_gesture.location and prev_gesture.location != None):
+            end_time = Gesture.Time
+            Chord.duration = end_time - start_time
+            return Chord 
+        prev_gesture = Chord
+    return
 
 #map each defined gesture to a note
 def create_gesture_dict(gesture_list, chord_list): 
@@ -46,13 +71,6 @@ def create_zone_map(chord_type_list):
         zone_map.append(chord_type) 
     return zone_map 
 
-#now given a gesture and given a zone can get chord note and type
-class chord:
-    def __init__(self, gesture, gesture_dict, velocity, duration,  zone, zone_map):
-        self.key = gesture_dict[gesture]
-        self.chord_type = zone_map[zone]
-        self.velocity = velocity
-
 gestures = ["thumbs_up", "thumbs_down", "peace", "love", "spock", "guh", "meep"]
 
 zone_map = create_zone_map(chord_type_list)
@@ -66,34 +84,6 @@ print("the gesture dict")
 print(gesture_dict)
 print("the chord propertires:")
 print(vars(my_first_chord))
-
-# To use the difference in location to calculate the velocity of the strum
-# Get the object where the strum is at either a maxima and minima n a certain divison of the screen
-def get_velocity(location):
-    velocities = []
-    for i in range(1, len(location)):
-        # Extracting x and y coordinates for the wrist landmark (index 0)
-        current_location = location[i][0]
-        previous_location = location[i-1][0]
-        
-        current_position = (
-            current_location.x, 
-            current_location.y
-        )
-        previous_position = (
-            previous_location.x, 
-            previous_location.y
-        )
-        
-        time_interval = end_time - start_time
-        displacement = [
-            current_location[j] - previous_location[j] for j in range(2) # displacement in x and y directions
-        ]
-        
-        velocity = [displacement[j] / time_interval for j in range(2)]
-        velocities.append(velocity)
-        
-    return velocities
 
 for chord in chord_list:
     play(chord=, duration=1)#duration currently set as 1. But in the future, it will be calculate via: end_time - start_time
