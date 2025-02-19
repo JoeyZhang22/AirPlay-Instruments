eacb0831febb602adf07eeb825ae4397c3832f2b
Joey Zhang <zjoey049@gmail.com>
2025-02-19 17:30:10 -0500

    update percussion inst changes into swift pythonbackend folder

--- airplayInstClient2/AirplayInst/AirplayInst/PythonBackEnd/MediaPipe/opencv_utils.py
+++ airplayInstClient2/AirplayInst/AirplayInst/PythonBackEnd/MediaPipe/opencv_utils.py
@@ -1,5 +1,6 @@
 import cv2
 import MediaPipe.mediapipe_utils as mediapipe_utils
+import numpy as np
 
 # Window parameters
 top_margin = 25  # pixels
@@ -16,7 +17,7 @@ label_font_size = 0.5
 label_font_thickness = 1
 
 # Division lines parameters
-line_color = (255, 0, 0)  # blue
+line_color = (230, 216, 173)  # light blue
 line_thickness = 1
 
 
@@ -47,8 +48,6 @@ def draw_text(
     )[0]
     text_width, text_height = text_size
 
-    # Boudary protection
-
     # Draw the text
     cv2.putText(
         current_frame,
@@ -72,20 +71,33 @@ def draw_gesture_labels(recognition_result, current_frame):
         y_min = min([landmark.y for landmark in hand_landmarks])
         y_max = max([landmark.y for landmark in hand_landmarks])
 
+        # Avoid drawing outside of boundary
+        text_x = x_min
+        text_y = y_max if y_min < 0.5 else y_min
+
+        offset_y = 1 if y_min < 0.5 else -1
+        offset_x = 0 if x_min < 0.5 else -1
+
         # Convert normalized coordinates to pixel values
         frame_height, frame_width = current_frame.shape[:2]
-        x_min_px = int(x_min * frame_width)
-        y_min_px = int(y_min * frame_height)
+        text_x_px = int(text_x * frame_width)
+        text_y_px = int(text_y * frame_height)
 
         # Get gesture classification results
         handedness = hand_result["Handedness"]
         gesture = hand_result["Gesture_Type"]
         score = hand_result["Score"]
-        result_text = f"{handedness} {gesture} ({score})"
+        result_text = f"{handedness} {gesture}"
+
+        # Compute result_text size
+        text_size = cv2.getTextSize(
+            result_text, cv2.FONT_HERSHEY_DUPLEX, default_font_size, default_font_thickness
+        )[0]
+        text_width, text_height = text_size
 
         draw_text(
             current_frame,
-            (x_min_px, y_min_px - 10),
+            (text_x_px + offset_x * text_width, text_y_px + offset_y * text_height),
             result_text,
             label_text_color,
             label_font_size,
@@ -94,9 +106,16 @@ def draw_gesture_labels(recognition_result, current_frame):
         # Draw Area Name
         area_text = f'{hand_result["Area"]}'
         area_text_color = (0, 0, 255)  # red
+
+        # Compute result_text size
+        text_size = cv2.getTextSize(
+            area_text, cv2.FONT_HERSHEY_DUPLEX, default_font_size, default_font_thickness
+        )[0]
+        text_width, text_height = text_size
+
         draw_text(
             current_frame,
-            (x_min_px, y_min_px - 25),
+            (text_x_px + offset_x * text_width, text_y_px + offset_y * text_height * 2),
             area_text,
             area_text_color,
             label_font_size,
@@ -104,6 +123,10 @@ def draw_gesture_labels(recognition_result, current_frame):
 
         # Draw hand landmarks on the frame
         mediapipe_utils.draw_landmarks(current_frame, hand_landmarks)
+
+        # Test Finger up
+        finger_status = mediapipe_utils.is_finger_up(hand_landmarks)
+        hand_result["Finger_Status"] = finger_status
 
 
 def draw_division_lines(current_frame, areas):
@@ -122,23 +145,65 @@ def draw_division_lines(current_frame, areas):
     frame_height, frame_width = current_frame.shape[:2]
 
     # Draw the mid line
-    cv2.line(
-        current_frame,
-        (frame_width // 2, 0),
-        (frame_width // 2, frame_height),
-        line_color,
-        line_thickness,
-    )
+    if areas[0].type is "Rectangle":
+        draw_dashed_line(
+            current_frame,
+            (frame_width // 2, 0),
+            (frame_width // 2, frame_height),
+            line_color,
+            line_thickness,
+        )
 
     # Draw Labels for each area
-    area_label_color = (0, 255, 0)  # blue
+    area_label_color = (88, 233, 88)  # light green
     for area in areas:
-        x_min = int(area.x_min * frame_width)
-        x_max = int(area.x_max * frame_width)
-        y_min = int(area.y_min * frame_height)
-        y_max = int(area.y_max * frame_height)
+        if area.type is "Rectangle":
+            x_min = int(area.x_min * frame_width)
+            x_max = int(area.x_max * frame_width)
+            y_min = int(area.y_min * frame_height)
+            y_max = int(area.y_max * frame_height)
 
-        cv2.line(
-            current_frame, (x_min, y_max), (x_max, y_max), line_color, line_thickness
-        )
-        draw_text(current_frame, (x_min, y_max - 5), area.name, area_label_color, 0.5)
+            draw_dashed_line(current_frame, (x_min, y_max), (x_max, y_max), line_color, line_thickness)
+            draw_text(current_frame, (x_min + 2, y_max - 5), area.name, area_label_color, 0.5)
+        elif area.type is "Circle" or area.type is "Corner":
+            normalized_x, normalized_y = area.center
+            center_x = int(normalized_x * frame_width)
+            center_y = int(normalized_y * frame_height)
+
+            radius_x = int(area.radius * frame_width)
+            radius_y = int(area.radius * frame_height)
+            
+            cv2.ellipse(current_frame, (center_x, center_y), (radius_x, radius_y), 0, 0, 360, line_color, line_thickness)
+
+            # adust area text based on position
+            text_offset_x = 1 if normalized_x < 0.5 else -1
+            text_offset_y = 1 if normalized_y < 0.5 else -1
+
+            # Compute text size
+            text_size = cv2.getTextSize(
+                area.name, cv2.FONT_HERSHEY_DUPLEX, default_font_size, default_font_thickness
+            )[0]
+            text_width, text_height = text_size
+
+            if area.type is "Circle":
+                draw_text(current_frame, (center_x, center_y + text_offset_y*(radius_y + text_height)), area.name, area_label_color, 0.5)
+            else:
+                draw_text(current_frame, (center_x + text_offset_x*(text_width), center_y + text_offset_y*(radius_y + text_height)), area.name, area_label_color, 0.5)
+
+def draw_dashed_line(image, start_point, end_point, color, thickness, dash_length=5, gap_length=5):
+    x1, y1 = start_point
+    x2, y2 = end_point
+    length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    num_dashes = int(length // (dash_length + gap_length))
+    
+    for i in range(num_dashes + 1):
+        start_x = int(x1 + (x2 - x1) * (i * (dash_length + gap_length)) / length)
+        start_y = int(y1 + (y2 - y1) * (i * (dash_length + gap_length)) / length)
+        end_x = int(x1 + (x2 - x1) * ((i * (dash_length + gap_length)) + dash_length) / length)
+        end_y = int(y1 + (y2 - y1) * ((i * (dash_length + gap_length)) + dash_length) / length)
+        
+        # Ensure the end of the dash doesn't exceed the endpoint
+        if i == num_dashes:
+            end_x, end_y = x2, y2
+        
+        cv2.line(image, (start_x, start_y), (end_x, end_y), color, thickness)
