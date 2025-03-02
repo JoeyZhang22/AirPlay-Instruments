31a68670372c9b05972fd8ecccae7878031baa46
Tangia Zhou <85449931+tangiazhou@users.noreply.github.com>
2025-03-02 14:19:17 -0500

    Update function to convert chord to MIDI ints

--- airplayinst/airplayinst/PythonBackEnd/MIDI/chord_player.py
+++ airplayinst/airplayinst/PythonBackEnd/MIDI/chord_player.py
@@ -8,23 +8,33 @@ from time import sleep
 """Need to expand on the this function to essentially parse a string array of various chords and map gestures to corresponding MIDI chords"""
 
 
+# def convert_chord_to_midi_chord(chord_list):
+#     midi_chords = []  # integers
+#     add_octave = 0
+#     for chord in chord_list:
+
+#         # collect chord_notes(ie. C-1,E-4,A-4,etc.)
+#         chord_notes = NoteContainer(chords.from_shorthand(chord))
+
+#         # convert notes to MIDI integer
+#         chord_ints = [
+#             int(note) + add_octave for note in chord_notes
+#         ]  # add int 12 to ensure proper C-4 key
+
+#         # Append chord_ints to midi_chordds
+#         midi_chords.append(chord_ints)
+
+#     return midi_chords
+
 def convert_chord_to_midi_chord(chord_list):
-    midi_chords = []  # integers
+    midi_chord_dict = {}
     add_octave = 0
+
     for chord in chord_list:
-
-        # collect chord_notes(ie. C-1,E-4,A-4,etc.)
         chord_notes = NoteContainer(chords.from_shorthand(chord))
-
-        # convert notes to MIDI integer
-        chord_ints = [
-            int(note) + add_octave for note in chord_notes
-        ]  # add int 12 to ensure proper C-4 key
-
-        # Append chord_ints to midi_chordds
-        midi_chords.append(chord_ints)
-
-    return midi_chords
+        chord_ints = [int(note) + add_octave for note in chord_notes]
+        midi_chord_dict[chord] = chord_ints
+    return midi_chord_dict
 
 
 def note_on(note, velocity=64, time=2):
